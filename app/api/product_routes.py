ff8652bd73a6385058a654686afb1ec8e02ac99d
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-05-24 12:34:35 -0700

    Added product id route

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -4,7 +4,16 @@ from app.models import Product
 product_routes = Blueprint("products", __name__)
 
 @product_routes.route('/')
-def product_list():
+def get_product_list():
     products = Product.query.all()
     return {products: [product.to_dict() for product in products]}
 
+@product_routes.route('/<id>')
+def get_product(id):
+    product = Product.query.get(id)
+    return {product: product.to_dict()}
+
+@product_routes.route('/<id>/reviews')
+def get_product_reviews(id):
+    # reviews = Product.query.get(id)
+    pass
