45388b63b92ad57a2ffb537e34de94d94a309a2f
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-05-24 12:55:18 -0700

    Added placeholder category route

---
+++ app/api/category_routes..py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+from app.models import Category, Product
+
+category_routes = Blueprint("categories", __name__)
+
+@category_routes.route('/<name>')
+def get_product_from_category(name):
+    products = Product.query.filter(Category.name == name).all()
+    return {products: [product.to_dict() for product in products]}
