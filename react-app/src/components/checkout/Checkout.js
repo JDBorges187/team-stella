4ec25f9788f1d061f27ebf0ee5e00b09d6402aad
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-05-24 14:51:14 -0700

    Adding basic css boxes

--- react-app/src/components/checkout/Checkout.js
+++ react-app/src/components/checkout/Checkout.js
@@ -7,11 +7,13 @@ const Checkout = () => {
         <>
             <h1>Checkout Page</h1>
             <div className='checkout_main_container'>
-                <div className='checkout_left_container'>
-                    <CheckoutForm />
-                </div>
-                <div className='checkout_right_container'>
-                    <CheckoutList />
+                <div className='checkout_center_container'>
+                    <div className='checkout_left_container'>
+                        <CheckoutForm />
+                    </div>
+                    <div className='checkout_right_container'>
+                        <CheckoutList />
+                    </div>
                 </div>
             </div>
         </>
