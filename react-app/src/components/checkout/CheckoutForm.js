1d0d4ea0ac566cc6d440daa7102a7fa9f5ffd73c
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-05-24 16:04:53 -0700

    Adding return to cart button

--- react-app/src/components/checkout/CheckoutForm.js
+++ react-app/src/components/checkout/CheckoutForm.js
@@ -9,25 +9,46 @@ const CheckoutForm = () => {
         e.preventDefault();
         history.push('/')
     }
+
+    const returnCart = (e) => {
+        e.preventDefault();
+        history.push('/cart')
+    }
+
     return (
         <>
             <h1>Checkout Page</h1>
-            <form onSubmit={submitCheckout}>
-                <div>
-                    {errors.map((error) => (
-                    <div>{error}</div>
-                    ))}
-                </div>
-                <div>
-                    <label htmlFor='address'>Address</label>
-                    <input name='address' type='text' />
-                </div>
-                <div>
-                    <label htmlFor='city'>City</label>
-                    <input name='city' type='text' />
-                </div>
-                <button type='submit'>Checkout</button>
-            </form>
+            <div className='checkout-form-container'>
+                <form onSubmit={submitCheckout}>
+                    <div>
+                        {errors.map((error) => (
+                            <div>{error}</div>
+                            ))}
+                    </div>
+                    <div>
+                        <label htmlFor='address'>Address</label>
+                        <input name='address' type='text' />
+                    </div>
+                    <div>
+                        <label htmlFor='city'>City</label>
+                        <input name='city' type='text' />
+                    </div>
+                    <div>
+                        <label htmlFor='country'>Country</label>
+                        <input name='country' type='text' />
+                    </div>
+                    <div>
+                        <label htmlFor='state'>State</label>
+                        <input name='state' type='text' />
+                    </div>
+                    <div>
+                        <label htmlFor='zip'>Zipcode</label>
+                        <input name='zip' type='text' />
+                    </div>
+                    <button type='submit'>Checkout</button>
+                    <button onClick={returnCart}>Return to Cart</button>
+                </form>
+            </div>
         </>
     )
 }
