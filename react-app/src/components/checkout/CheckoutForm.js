ad953f6599c4fe61d313f4e5813bf6b897032833
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-05-24 23:38:57 -0700

    Added quantity span

--- react-app/src/components/checkout/CheckoutForm.js
+++ react-app/src/components/checkout/CheckoutForm.js
@@ -4,10 +4,16 @@ import {useHistory} from 'react-router-dom'
 //Form is mostly just for show, db functionality would be a bonus?
 const CheckoutForm = () => {
     const [errors, setErrors] = useState([]);
+    const [address, setAddress] = useState("");
+    const [city, setCity] = useState("")
+    const [country, setCountry] = useState("")
+    const [state, setState] = useState("")
+    const [zipcode, setZipcode] = useState("")
     const history = useHistory();
 
     const submitCheckout = (e) => {
         e.preventDefault();
+        setErrors([]);
         history.push('/')
     }
 
@@ -28,23 +34,23 @@ const CheckoutForm = () => {
                     </div>
                     <div>
                         <label htmlFor='address'>Address</label>
-                        <input name='address' type='text' />
+                        <input name='address' type='text' value={address} onChange={e => setAddress(e.target.value)}/>
                     </div>
                     <div>
                         <label htmlFor='city'>City</label>
-                        <input name='city' type='text' />
+                        <input name='city' type='text' value={city} onChange={e => setCity(e.target.value)}/>
                     </div>
                     <div>
                         <label htmlFor='country'>Country</label>
-                        <input name='country' type='text' />
+                        <input name='country' type='text' value={country} onChange={e => setCountry(e.target.value)}/>
                     </div>
                     <div>
                         <label htmlFor='state'>State</label>
-                        <input name='state' type='text' />
+                        <input name='state' type='text' value={state} onChange={e => setState(e.target.value)}/>
                     </div>
                     <div>
                         <label htmlFor='zip'>Zipcode</label>
-                        <input name='zip' type='text' />
+                        <input name='zip' type='text' value={zipcode} onChange={e => setZipcode(e.target.value)}/>
                     </div>
                     <button type='submit'>Checkout</button>
                     <button onClick={returnCart}>Return to Cart</button>
--- react-app/src/components/checkout/CheckoutList.js
+++ react-app/src/components/checkout/CheckoutList.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import CheckoutItem from './CheckoutItem'
 
+//Will want to get list of products from redux store
 const CheckoutList = () => {
+    let totalPrice = 135.00;
     return (
         <>
             <ul className='checkout-list'>
-                <li><CheckoutItem name='Product 1' price={20.00}/></li>
-                <li><CheckoutItem name='Product 2' price={25.00}/></li>
+                {/* Add a loop through products here*/}
+                <li><CheckoutItem name='Product 1' quantity={1} price={20.00}/></li>
+                <li><CheckoutItem name='Product 2' quantity={2} price={25.00}/></li>
+                <li><CheckoutItem name='Product 3' quantity={1} price={35.00}/></li>
+                <li><CheckoutItem name='Product 4' quantity={2} price={15.00}/></li>
             </ul>
 
             <div className='divider'/>
@@ -16,7 +21,7 @@ const CheckoutList = () => {
                     <tr>
                         <th>Total: </th>
                         <td>
-                            <span>Insert price here</span>
+                            <span>${totalPrice.toFixed(2)}</span>
                         </td>
                     </tr>
                 </tbody>
