c207c46f5dd1eeac0956b3545a79d12baddf6370
Alex Rodriguez <alex.rodriguezsntg@gmail.com>
2021-05-26 10:57:52 -0400

    Added shopping cart open and closing animation

--- react-app/src/components/navbar/CartDropdown.js
+++ react-app/src/components/navbar/CartDropdown.js
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import "./Navbar.css";
 import usePrevious from "../../utilities/usePrevious";
+const classNames = require("classnames");
 
 const CartDropdown = () => {
   const cart = useSelector((state) => state.cart);
   const products = useSelector((state) => state.products);
   const [cartDropdownVisible, setCartDropdownVisible] = useState(false);
+  const [cartCloseClass, setCartCloseClass] = useState(false);
   const closeCartDropdown = () => {
-    setCartDropdownVisible(false);
+    setCartCloseClass(true);
+    setTimeout(() => setCartDropdownVisible(false), 1000);
   };
 
   const getTotalItems = (cart) =>
@@ -19,16 +22,25 @@ const CartDropdown = () => {
   useEffect(() => {
     const totalCount = getTotalItems(cart);
     if (totalCount > prevTotalItems) {
+      setCartCloseClass(false);
       setCartDropdownVisible(true);
       console.log("OPEN DROPDOWN");
     }
   }, [cart]);
 
+  const cartDropdownClass = classNames({
+    "cart-dropdown": cartDropdownVisible,
+    "cart-close": cartCloseClass,
+  });
+
   return (
     <>
       {cartDropdownVisible && (
         <div className="close-cart-dropdown" onClick={closeCartDropdown}>
-          <div className="cart-dropdown" onClick={(e) => e.stopPropagation()}>
+          <div
+            className={cartDropdownClass}
+            onClick={(e) => e.stopPropagation()}
+          >
             {Object.entries(cart).map(([key, value]) => {
               return (
                 <div className="cart-dropdown-item" key={key}>
