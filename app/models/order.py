25bf5ab3c6596c9d4cdfdd5180b855de267525ed
Alex Rodriguez <alex.rodriguezsntg@gmail.com>
2021-05-26 17:50:00 -0400

    Added orders and orer_items table with relationships

---
+++ app/models/order.py
@@ -0,0 +1,10 @@
+from .db import db
+
+
+class Order(db.Model):
+    __tablename__ = "orders"
+
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    order_items = db.relationship("OrderItem", backref="order", lazy=True)
--- app/models/product.py
+++ app/models/product.py
@@ -1,17 +1,19 @@
 from .db import db
 
+
 class Product(db.Model):
     __tablename__ = 'products'
 
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(40), nullable = False)
-    price = db.Column(db.Float, nullable = False)
-    description = db.Column(db.Text, nullable = False)
-    image = db.Column(db.String(300), nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    image = db.Column(db.String(300), nullable=False)
     categoryId = db.Column(db.Integer, db.ForeignKey("categories.id"))
 
     category = db.relationship("Category", back_populates="product")
     product_reviews = db.relationship("Review", back_populates="product")
+    product_orders = db.relationship("OrderItem", backref="product", lazy=True)
 
     def to_dict(self):
         return {
