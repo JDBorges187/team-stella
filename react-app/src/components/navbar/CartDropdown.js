34156ffca8327a35f4a47c46f5afb428e924c678
Alex Rodriguez <alex.rodriguezsntg@gmail.com>
2021-05-26 12:32:34 -0400

    Added add and remove buttons to cart and works with store

--- react-app/src/components/navbar/CartDropdown.js
+++ react-app/src/components/navbar/CartDropdown.js
@@ -1,23 +1,33 @@
 import React, { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import "./Navbar.css";
 import usePrevious from "../../utilities/usePrevious";
+import { addCartItem, removeCartItem } from "../../store/cart";
 const classNames = require("classnames");
 
 const CartDropdown = () => {
+  const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart);
   const products = useSelector((state) => state.products);
   const [cartDropdownVisible, setCartDropdownVisible] = useState(false);
   const [cartCloseClass, setCartCloseClass] = useState(false);
   const closeCartDropdown = () => {
     setCartCloseClass(true);
-    setTimeout(() => setCartDropdownVisible(false), 1000);
+    setTimeout(() => setCartDropdownVisible(false), 500);
   };
 
   const getTotalItems = (cart) =>
     Object.keys(cart).reduce((sum, itemID) => cart[itemID] + sum, 0);
 
   const prevTotalItems = usePrevious(getTotalItems(cart));
+
+  const addOne = (product) => {
+    dispatch(addCartItem(product));
+  };
+
+  const removeOne = (product) => {
+    dispatch(removeCartItem(product));
+  };
 
   useEffect(() => {
     const totalCount = getTotalItems(cart);
@@ -44,7 +54,22 @@ const CartDropdown = () => {
             {Object.entries(cart).map(([key, value]) => {
               return (
                 <div className="cart-dropdown-item" key={key}>
-                  {`${products[key].name} = ${value}`}
+                  {`${products[key].name}`}
+                  <button
+                    style={{ padding: 3 }}
+                    onClick={() => addOne(products[key])}
+                    className="add-one"
+                  >
+                    +
+                  </button>
+                  <span>{value}</span>
+                  <button
+                    style={{ padding: 3 }}
+                    onClick={() => removeOne(products[key])}
+                    className="remove-one"
+                  >
+                    -
+                  </button>
                   <span>{products[key].price}</span>
                 </div>
               );
