adf119e67e78091484ff3c075d561f816a2c3a17
Ondre <ondre.williams.77@gmail.com>
2021-05-26 08:58:10 -0500

    pull from main

--- react-app/src/components/ProductDetail/index.js
+++ react-app/src/components/ProductDetail/index.js
@@ -6,25 +6,44 @@ import './ProductDetail.css';
 
 const ProductDetail = () => {
 
-    // const { id } = useParams();
+    const { id } = useParams();
+    console.log("********************");
+    console.log(id);
+    const dispatch = useDispatch();
 
-    // const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getOneProduct(id));
+    }, [dispatch, id]);
 
-    // useEffect(() => {
-    //     dispatch(getOneProduct(id));
-    // }, [dispatch, id]);
+    const product = useSelector(state => state.products[id]);
 
-    // const product = useSelector(state => state.products[id]);
+    if (!product) {
+        return null;
+    }
 
-    // if (!product) {
-    //     return null;
+    // const product = {
+    //     name: 'Replica Monster Energy Yamaha Team 2021 T-shirt',
+    //     price: 56.00,
+    //     description: 'Official replica of the polo shirt that will be worn by the Yamaha Monster Energy Team and its official riders: the Spanish Maverick Viñales and the Frenchman Fabio Quartararo in the 2021 season. It is made of technical fabric.',
+    //     image: 'https://res.cloudinary.com/dpf7crjn5/image/upload/v1621893530/test%20data/teamMonsterShirt_d1mhot.jpg',
     // }
 
-    const product = {
-        name: 'Replica Monster Energy Yamaha Team 2021 T-shirt',
-        price: 56.00,
-        description: 'Official replica of the polo shirt that will be worn by the Yamaha Monster Energy Team and its official riders: the Spanish Maverick Viñales and the Frenchman Fabio Quartararo in the 2021 season. It is made of technical fabric.',
-        image: 'https://res.cloudinary.com/dpf7crjn5/image/upload/v1621893530/test%20data/teamMonsterShirt_d1mhot.jpg',
+
+    const showHideDesc = (infoClass, arrowClass, buttonClass) =>{
+        let chev = document.querySelector(arrowClass);
+        let btn = document.querySelector(buttonClass);
+
+        if (document.querySelector(infoClass).style.display !== "block"){
+            document.querySelector(infoClass).style.display = "block";
+            chev.style.transform = "rotate(180deg)";
+            btn.style.borderBottom = "none";
+        }
+        else{
+            document.querySelector(infoClass).style.display = "none";
+            chev.style.transform = "rotate(-360deg)";
+            btn.style.borderBottom = "1px solid darkgray"
+        }
+
     }
 
     return (
@@ -55,7 +74,18 @@ const ProductDetail = () => {
                 </div>
             </div>
             <div className="pdt-dtl__xtra-info">
-                <p className="pdt-dtl__desc">{product.description}</p>
+                <button onClick={() => showHideDesc(".pdt-dtl__desc", ".pdt-dtl__desc-chevron", ".desc-btn")} className="pdt-dtl__xtra-info-btn desc-btn">
+                    <span className="pdt-dtl__xtra-info-title">Description</span>
+                    <i className="fas fa-chevron-down pdt-dtl__desc-chevron"></i>
+                </button>
+                <div className="pdt-dtl__desc">{product.description}</div>
+                <button onClick={() => showHideDesc(".pdt-dtl__reviews", ".pdt-dtl__reviews-chevron", ".reviews-btn" )} className="pdt-dtl__xtra-info-btn reviews-btn">
+                    <span className="pdt-dtl__xtra-info-title">Reviews</span>
+                    <i className="fas fa-chevron-down pdt-dtl__reviews-chevron"></i>
+                </button>
+                <div className="pdt-dtl__reviews">
+                    Some reviews...
+                </div>
             </div>
         </div>
     );
