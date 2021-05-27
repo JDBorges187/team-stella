855ab0c06f08fc90c8052b446e8817c41a7d77b1
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-05-27 14:43:06 -0700

    Added add to cart button on product card on home page

--- react-app/src/components/main/ProductCard.js
+++ react-app/src/components/main/ProductCard.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import { useDispatch } from "react-redux";
 import { addCartItem } from "../../store/cart";
 
 const ProductCard = ({ product }) => {
+  const [showAdd, setShowAdd] = useState(false);
   const dispatch = useDispatch();
   const onPurchase = (e) => {
     e.stopPropagation();
@@ -10,10 +11,14 @@ const ProductCard = ({ product }) => {
   };
 
   return (
-    <div className="card-container" onClick={(e) => onPurchase(e)}>
-      <div className="card-image" style={{backgroundImage: `url(${product.image})`}}>
-        {/* <img src={`${product.image}`}></img> src={`${product.image}`} */}
-      </div>
+    <div className="card-container"
+      onMouseOver={() => setShowAdd(true)}
+      onMouseLeave={() => setShowAdd(false)}
+    >
+      <i onClick={(e) => onPurchase(e)}
+        className={'fas fa-plus-circle fa-2x card-container_add-cart' + (showAdd ? '' : ' hide-add')}
+      />
+      <div className="card-image" style={{backgroundImage: `url(${product.image})`}} />
       <div className="card-title">{product.name}</div>
       <div className="card-price">{`$${product.price.toFixed(2)}`}</div>
     </div>
