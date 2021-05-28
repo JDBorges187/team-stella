db4b46d989e2f2f4fadccc7f47faaed3c4c6085e
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-05-28 09:05:36 -0700

    Added fetch request

--- react-app/src/components/orders/Orders.js
+++ react-app/src/components/orders/Orders.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import OrderSelect from './OrderSelect'
 import OrderSummary from './OrderSummary'
 
@@ -7,8 +7,15 @@ const Orders = () => {
     const [orderId, setOrderId] = useState(-1);
 
     const getOrders = async () => {
-        let response = await fetch();
+        let response = await fetch('/api/orders');
+        const order_list = await response.json();
+        setOrders(order_list);
+        console.log(order_list);
     }
+
+    useEffect( () => {
+        getOrders();
+    }, []);
 
     return (
         <>
