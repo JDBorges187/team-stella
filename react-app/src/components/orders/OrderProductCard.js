b5a145e89ca9d08f5b73a8fc9360209f23d6c2ba
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-05-28 14:36:59 -0700

    Adding image to product card

--- react-app/src/components/orders/OrderProductCard.js
+++ react-app/src/components/orders/OrderProductCard.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import {useHistory} from 'react-router-dom';
 
 const OrderProductCard = ({item, products}) => {
     let product = products[item.productId]
+    const history = useHistory();
+    const goToProduct = () => {
+        history.push(`/products/${item.productId}`)
+    }
+
     return (
-        <div className='order-product-card'>
+        <div className='order-product-card' onClick={goToProduct}>
             {item.orderid}
             <div className="order-product-card-image" style={{backgroundImage: `url(${product.image})`}} />
             <div className="order-product-card-title">{product.name}</div>
-             <div className="order-product-card-price">{`$${product.price.toFixed(2)}`}</div>
+            <div className="order-product-card-price">{`$${product.price.toFixed(2)}`}</div>
+            <div className="order-product-card-quantity"></div>
         </div>
     )
 }
--- react-app/src/components/orders/OrderSelect.js
+++ react-app/src/components/orders/OrderSelect.js
@@ -4,15 +4,20 @@ const OrderSelect = ({orders, setOrderId}) => {
 
     return (
         <div className='order-select-sidebar'>
-            {orders && orders.map((order, idx) => {
-                return (
-                    <div className='order-list-item' key={idx}
-                    onClick={() => setOrderId(idx)}
-                    >
-                        {orderDateFormat(new Date(order.createdAt))}
-                    </div>
-                )
-            }) }
+            <div className='order-select-sidebar-header'>
+                Previous Orders:
+            </div>
+            <div className='order-select-list'>
+                {orders && orders.map((order, idx) => {
+                    return (
+                        <div className='order-list-item' key={idx}
+                        onClick={() => setOrderId(idx)}
+                        >
+                            {orderDateFormat(new Date(order.createdAt))}
+                        </div>
+                    )
+                }) }
+            </div>
         </div>
     )
 }
