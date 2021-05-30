7cc0e5364ca926be3ff59ca45978cb88d5d51cdd
Alex Rodriguez <alex.rodriguezsntg@gmail.com>
2021-05-30 15:15:43 -0400

    Added link to featured items

--- react-app/src/components/main/Featured.js
+++ react-app/src/components/main/Featured.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import {Link} from "react-router-dom"
 import merchImg from "../../pictures/merch-stock.jpg";
 
 const Featured = () => {
@@ -13,9 +14,11 @@ const Featured = () => {
       {featuredProducts.map((product) => {
         return (
           <div key={product.id} className="featured-product">
+            <Link to={`/products/${product.id}`}>
             <div className="featured-product-img">
               <img src={merchImg}></img>
             </div>
+            </Link>
             <h3>{product.name}</h3>
           </div>
         );
