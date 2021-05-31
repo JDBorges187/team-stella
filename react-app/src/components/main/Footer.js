0526459b737a53f7a375bc740f5dad9de11e6ae5
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-05-31 00:14:16 -0700

    Filling out footer

--- react-app/src/components/main/Footer.js
+++ react-app/src/components/main/Footer.js
@@ -1,7 +1,44 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 
 const Footer = () => {
-  return <div className="footer">FOOTER</div>;
+  return <div className="footer">
+      <div className="profile-section">
+        <h2>Alex Rodriguez</h2>
+        <ul className="profile-links">
+          <li>
+            <a href="https://github.com/alex-therookie">Github</a>
+          </li>
+        </ul>
+      </div>
+      <div className="profile-section">
+        <h2>Brian Xue</h2>
+        <ul className="profile-links">
+          <li>
+              <a href="https://github.com/bxue2">Github</a>
+          </li>
+          <li>
+              <a href="https://www.linkedin.com/in/brian-xue/">LinkedIn</a>
+          </li>
+        </ul>
+      </div>
+      <div className="profile-section">
+        <h2>Jorge Borges</h2>
+        <ul className="profile-links">
+          <li>
+              <a href="https://github.com/JDBorges187">Github</a>
+          </li>
+        </ul>
+      </div>
+      <div className="profile-section">
+        <h2>Ondre Williams</h2>
+        <ul className="profile-links">
+          <li>
+              <Link to="https://github.com/OndreWilliams">Github</Link>
+          </li>
+        </ul>
+      </div>
+  </div>;
 };
 
 export default Footer;
