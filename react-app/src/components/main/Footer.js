9b51628ece73cbfaac77e06b9c37073016dae88e
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-07-01 15:13:38 -0700

    Styled all footer links

--- react-app/src/components/main/Footer.js
+++ react-app/src/components/main/Footer.js
@@ -3,7 +3,6 @@ import alexImage from '../../pictures/alex-headshot.jpg'
 import brianImage from '../../pictures/brian-headshot.png'
 import jorgeImage from '../../pictures/jorge-headshot.png'
 import ondreImage from '../../pictures/ondre-headshot.jpeg'
-import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return <div className="footer">
@@ -12,50 +11,30 @@ const Footer = () => {
         <img className="profile-image" src={alexImage} />
       </div>
       <h2>Alex Rodriguez</h2>
-      <ul className="profile-links">
-        <li>
-          <i class="fab fa-github"></i>
-          <a href="https://github.com/alex-therookie">Github</a>
-        </li>
-        <li>
-          <i class="fab fa-linkedin"></i>
-          <a href="https://www.linkedin.com/in/alex-rodriguez-8a8aa1146/">LinkedIn</a>
-        </li>
-      </ul>
+      <div className='profile-links'>
+        <a href="https://github.com/alex-therookie"><div class="profile-link_icon fab fa-github fa-3x"></div></a>
+        <a href="https://www.linkedin.com/in/alex-rodriguez-8a8aa1146/"><div class="profile-link_icon fab fa-linkedin fa-3x"></div></a>
+      </div>
     </div>
     <div className="profile-section">
       <div className='profile-image-container'>
         <img className="profile-image" src={brianImage} />
       </div>
       <h2>Brian Xue</h2>
-      <ul className="profile-links">
-        <li>
-          <i class="fab fa-github"></i>
-          <a href="https://github.com/bxue2">Github</a>
-        </li>
-        <li>
-          <i class="fab fa-linkedin"></i>
-          <a href="https://www.linkedin.com/in/brian-xue/">LinkedIn</a>
-        </li>
-      </ul>
+      <div className='profile-links'>
+        <a href="https://github.com/bxue2"><div class="profile-link_icon fab fa-github fa-3x"></div></a>
+        <a href="https://www.linkedin.com/in/brian-xue/"><div class="profile-link_icon fab fa-linkedin fa-3x"></div></a>
+      </div>
     </div>
     <div className="profile-section">
       <div className='profile-image-container'>
         <img className="profile-image" src={jorgeImage} />
       </div>
       <h2>Jorge Borges</h2>
-      <ul className="profile-links">
-        <li>
-          <i class="fab fa-github"></i>
-          <a href="https://github.com/JDBorges187">Github</a>
-        </li>
-        <li>
-          <i class="fab fa-linkedin"></i>
-          <a href="https://www.linkedin.com/in/jorge-engineer/">LinkedIn</a>
-        </li>
-
-
-        </ul>
+      <div className='profile-links'>
+        <a href="https://github.com/JDBorges187"><div class="profile-link_icon fab fa-github fa-3x"></div></a>
+        <a href="https://www.linkedin.com/in/jorge-engineer/"><div class="profile-link_icon fab fa-linkedin fa-3x"></div></a>
+      </div>
     </div>
     <div className="profile-section">
       <div className='profile-image-container'>
@@ -66,12 +45,6 @@ const Footer = () => {
         <a href="https://github.com/OndreWilliams"><div class="profile-link_icon fab fa-github fa-3x"></div></a>
         <a href="https://www.linkedin.com/in/ondre-williams-289b26132/"><div class="profile-link_icon fab fa-linkedin fa-3x"></div></a>
       </div>
-      {/* <ul className="profile-links">
-        <li> */}
-        {/* </li>
-        <li> */}
-        {/* </li>
-      </ul> */}
     </div>
   </div>;
 };
